970e3b78085f0b6c5ef6e52af980a934bf554b38
Aginavee <114096319+Aginavee@users.noreply.github.com>
2022-09-21 22:35:49 +0530

    Add files via upload

---
+++ navaneeta.s
@@ -0,0 +1,86 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Grocery Shop</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Spartan:wght@400;500;600;700;900&display=swap" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <!-- Header Grover Grocery Section -->
+    <header class="_header_grover_grocery_wrapper">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light _grover_grocery_navbar">
+            <div class="container">
+                <div class="_logo">
+             
+                       <h2 class="logoText">GRO<span class="_header__clr">HOUSE</span></h2c>
+                  
+                </div>
+                <button class="navbar-toggler _toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0 _grover_grocery_list">
+                        <li class="nav-item _grover_grocery_list_item">
+                            <a class="nav-link _active _grover_grocery_list_link" aria-current="page" href="#0">Home</a>
+                        </li>
+                        <li class="nav-item _grover_grocery_list_item">
+                            <a class="nav-link _grover_grocery_list_link" href="#0">Menu</a>
+                        </li>
+                        <li class="nav-item _grover_grocery_list_item">
+                            <a class="nav-link _grover_grocery_list_link" href="#0" tabindex="-1" aria-disabled="true">Service</a>
+                        </li>
+                        <li class="nav-item _grover_grocery_list_item">
+                            <a class="nav-link _grover_grocery_list_link" href="#0" tabindex="-1" aria-disabled="true">Shop</a>
+                        </li>
+                        <li class="nav-item _grover_grocery_list_item">
+                            <a class="nav-link _grover_grocery_list_link" href="#0" tabindex="-1" aria-disabled="true">Sign In</a>
+                        </li>
+                    </ul>
+                   
+                               
+                               
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <div class="_header_grover_grocery_wrap">
+            <div class="container">
+               <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12">
+            <div class="_header_grover_grocery_content_img">
+                <img src="assets/images/feat.jpg" alt="Image" class="_feature_grover_img">   
+                            </div>
+                            <div class="_header_grover_grocery_content_txt"></div>
+                                <h1 class="_header_grover_grocery_content_title">Bring the <span class="_header_red_clr">Store</span> to your <span class="_header_red_clr">door</span></h1>
+                                <p class="_header_grover_grocery_content_para">Best Groceries app ever.Pick Your desired groceries from the menu</p>
+                                <a href="#0" class="_header_now_btn">How to Order</a>
+                            </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Grover Grocery Section Ends -->
+    <!-- Service Grover Grocery Section -->
+    
+    <!-- Footer Grover Grocery Section Ends -->
+    <!-- Bootstrap -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
